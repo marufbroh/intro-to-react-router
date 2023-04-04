e3e849494c3c86820f2a8f959fdc097fb1ed8d38
marufbroh <marufmd775@gmail.com>
2023-04-04 19:58:41 +0600

    active link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
         <nav style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-
-            {/* <NavLink
-                to={"/contacts"}
-                style={({ isActive }) => isActive ? { color: 'red' } : ''}
+            <NavLink
+                to={"/"}
+                className={({ isActive }) => isActive ? "active" : ""}
             >
-            </NavLink> */}
+                Home
+            </NavLink>
+            <NavLink
+                to={"/users"}
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Users
+            </NavLink>
+            <NavLink
+                to={"/posts"}
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Posts
+            </NavLink>
+            <NavLink
+                to={"/about"}
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                About
+            </NavLink>
+            <NavLink
+                to={"/contact"}
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Contact
+            </NavLink>
         </nav>
     );
 };
